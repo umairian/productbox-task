98ecb089a5db464ac8dd1f7a8507e50e9972c798
itsumairsyed@gmail.com <itsumairsyed@gmail.com>
2021-05-25 04:02:21 -0700

    Multi address functionality added

--- controllers/address.js
+++ controllers/address.js
@@ -3,9 +3,21 @@ const axios = require('axios');
 
 module.exports = (req, res, next) => {
     const address = req.query.address;
+    console.log(address);
 
-    axios.get(address).then(res => {
-        var title = res.data.split('<title>')[1].split('</title>')[0]
+    if(address.length > 1) {
+        const titlesArray = [];
+        address.forEach(element => {
+            axios.get(element).then(result => {
+                var title = result.data.split('<title>')[1].split('</title>')[0];
+                titlesArray.push(title);
+            }).catch();
+        });
+        return res.status(200).render(path.join(__dirname, "../", "views", "address.pug"), { address: address, title: titlesArray});
+    }
+
+    axios.get(address).then(result => {
+        var title = result.data.split('<title>')[1].split('</title>')[0];
         return res
         .status(200)
         .render(path.join(__dirname, "../", "views", "address.pug"), { address: address, title: title});
@@ -17,6 +29,7 @@ module.exports = (req, res, next) => {
             message: 'Request fetching failed'
         });
     });
+
 
       
 };
