be81a10b8079c90c12c9f21f83d1068d71cff154
itsumairsyed@gmail.com <itsumairsyed@gmail.com>
2021-05-26 13:21:57 -0700

    single address functionality added

--- app.js
+++ app.js
@@ -1,20 +1,51 @@
 const express = require('express');
 const app = express();
+const http = require('follow-redirects').http;
+const url = require('url');
 const port = 3000;
 
-// Configuring dynamic view files
-app.set('view engine', 'pug');
-app.set('views', 'views');
+app.get('/I/want/title/', (req, res, next) => {
+    const address = req.query.address;
 
-// I/want/title route
-const addressRoute = require('./routes/address');
+    var addressArray = [];
+    if(!Array.isArray(address)) { // Check whether address is array or not
+        addressArray.push(address);
+        console.log(addressArray);
+    } else {
+        addressArray = [...address];
+        console.log("ruungnign");
+    }
 
-app.use(addressRoute);
+    if(addressArray.length == 1) { // In case of single address
+        let data = [];
 
-// Error Handling: Invalid Requests will be handled by the following middleware 
-const notFoundRoute = require('./routes/404');
+        http.get(address, (resp) => {
+            
+            // A chunk of data has been received.
+            resp.on('data', (chunk) => {
+                data.push(chunk);
+                console.log(chunk);
+                
+            });
+          
+            // The whole response has been received. Print out the result.
+            resp.on('end', () => {
+                const result = Buffer.concat(data).toString('utf-8');
+                console.log(result);
+                var title = result.split('<title>')[1].split('</title>')[0];
+                console.log(title);
 
-app.use(notFoundRoute);
+                return res.send(`<ul><li>${address} - ${title}</li></ul>`);
+            });
+          
+          }).on("error", (err) => {
+            console.log("Error: " + err.message);
+          });
+        }
+});
 
+app.get('*', (req, res, next) => {
+    return res.send('<h1>Error 404, not found</h1>');
+});
 
-app.listen(port, () => console.log(`This app listening on port ${port}`));
+app.listen(port);
